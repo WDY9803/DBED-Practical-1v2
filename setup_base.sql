a1ad2bb0c5ef0ebabd7e01c8f59fe670b1b353f1
Dingyuan Wang <a1789431@adelaide.edu.au>
2024-08-18 13:30:31 +0800

    try

--- setup_base.sql
+++ setup_base.sql
@@ -3,8 +3,8 @@ CREATE DATABASE test1;
 USE test1;
 DROP TABLE IF EXISTS tab1;
 CREATE TABLE tab1 (
-    recordID INT(11) PRIMARY KEY,
-    NAME VARCHAR(30)
+    recordID INT (11),
+    name VARCHAR (30)
 );
 
 INSERT INTO tab1 (recordID, name) VALUES (1, 'Fred');
